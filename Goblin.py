62d1b6a4750da372c2cefeccae314ccb91e7e18d
Sanya <shitsk848@gmail.com>
2022-10-25 17:35:52 +0400

    q

--- Goblin.py
+++ Goblin.py
@@ -1,13 +1,15 @@
 import pygame
+from pygame.image import load
 GREEN=(56,45,32)
 
 WIN_WIDTH = 1334
 WIN_HEIGHT =750
+WHITE=(255,255,255)
 class Goblin(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image=pygame.Surface((50,35))
-        self.image.fill(GREEN)
+        self.image=load('images/hero/boss2.png')
+        self.image.set_colorkey(WHITE)
         self.rect=self.image.get_rect()
         self.rect.center=(WIN_WIDTH/2,WIN_HEIGHT/2)
     def update(self):
